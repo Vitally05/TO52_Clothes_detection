9154bfce07a01eeae0ed212265d08946275c620e
Elise Albrecht <118198011+Vitally05@users.noreply.github.com>
2025-01-29 11:06:30 -0500

    Cleaning repository

---
+++ Interface/Interface.py
@@ -0,0 +1,206 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, Canvas, NW
+from PIL import Image, ImageTk
+import Utilities as util
+
+class ClothesFinderApp:
+    def __init__(self):
+        self.selected_image_path = None
+        self.root = tk.Tk()
+        self.root.title("Clothes finder")
+        self.category = None
+        self.brand_vars = {}
+        self.size_vars = {}
+        self.height = 300
+        self.width = 200
+        self.canvas = None
+        self.similar_count_entry = None
+
+
+    def create_widgets(self, sizes, brands):
+        # Image Selection
+        image_frame = tk.Frame(self.root)
+        image_frame.pack(pady=10)
+        self.canvas = Canvas(image_frame, width=self.width, height=self.height)
+        self.canvas.pack()
+        choose_btn = tk.Button(image_frame, text="Choose Image", command=self.choose_image)
+        choose_btn.pack()
+
+        # Characteristics Section
+        char_frame = tk.Frame(self.root)
+        char_frame.pack(pady=10)
+
+        tk.Label(char_frame, text="Size").grid(row=0, column=0, padx=5)
+
+        for i, size in enumerate(sizes):
+            var = tk.BooleanVar()
+            cb = tk.Checkbutton(char_frame, text=size, variable=var)
+            cb.grid(row=i + 1, column=0, sticky='w')
+            self.size_vars[size] = var
+
+        # Button to Select All Sizes
+        select_all_btn = tk.Button(char_frame, text="Select All", command=lambda: self.select_all(self.size_vars))
+        select_all_btn.grid(row=len(sizes) + 1, column=0, pady=5)
+
+        # Button to Deselect All Sizes
+        deselect_all_btn = tk.Button(char_frame, text="Deselect All", command=lambda: self.deselect_all(self.size_vars))
+        deselect_all_btn.grid(row=len(sizes) + 2, column=0, pady=5)
+
+        tk.Label(char_frame, text="Brand").grid(row=0, column=1, padx=5)
+
+        for i, brand in enumerate(brands):
+            var = tk.BooleanVar()
+            cb = tk.Checkbutton(char_frame, text=brand, variable=var)
+            cb.grid(row=i + 1, column=1, sticky='w')
+            self.brand_vars[brand] = var
+
+        # Button to Select All Brands
+        select_all_btn = tk.Button(char_frame, text="Select All", command=lambda: self.select_all(self.brand_vars))
+        select_all_btn.grid(row=len(brands) + 1, column=1, pady=5)
+
+        # Button to Deselect All Brands
+        deselect_all_btn = tk.Button(char_frame, text="Deselect All",
+                                     command=lambda: self.deselect_all(self.brand_vars))
+        deselect_all_btn.grid(row=len(brands) + 2, column=1, pady=5)
+
+        tk.Label(char_frame, text="How many similar \n (between 1 and 12)").grid(row=0, column=2, padx=5)
+        self.similar_count_entry = tk.Entry(char_frame, width=5)
+        self.similar_count_entry.grid(row=1, column=2, padx=5)
+        self.similar_count_entry.insert(0, "6")  # Default value
+
+        # Find Button
+        find_btn = tk.Button(self.root, text="Find!", command=self.validate_and_find)
+        find_btn.pack(pady=10)
+
+    def get_selected_options(self):
+        selected_sizes = [size for size, var in self.size_vars.items() if var.get()]
+        selected_brands = [brand for brand, var in self.brand_vars.items() if var.get()]
+        return selected_sizes, selected_brands
+
+    def choose_image(self):
+        self.selected_image_path = filedialog.askopenfilename()
+        if self.selected_image_path:
+            img = Image.open(self.selected_image_path)
+            img.thumbnail((self.height, self.width))  # Resize image
+            img = ImageTk.PhotoImage(img)
+            self.canvas.create_image(10, 10, anchor=NW, image=img)
+            self.canvas.image = img  # Keep a reference to avoid garbage collection
+
+    def select_all(self, vars):
+        for var in vars.values():
+            var.set(True)
+
+    def deselect_all(self, vars):
+        for var in vars.values():
+            var.set(False)
+
+    def get_similar_count(self):
+        try:
+            count = int(self.similar_count_entry.get())
+            return count
+        except ValueError:
+            messagebox.showerror("Invalid input",
+                                 "Please enter a valid integer (between 1 and 12) for the number of similar items.")
+            return None
+
+    def check_similar_count(self):
+        similar_count = self.get_similar_count()
+        if similar_count is None:
+            return False
+        if similar_count < 1 or similar_count > 12:
+            messagebox.showerror("Invalid input", "Please enter a number between 1 and 12.")
+            return False
+        return True
+
+    def validate_and_find(self):
+        if self.check_similar_count():
+            similar_count = self.get_similar_count()
+            selected_sizes, selected_brands = self.get_selected_options()
+            if similar_count is not None:
+                print(f"Finding {similar_count} similar items...")
+                self.category, bbox = util.predict_category(self.selected_image_path)
+                if bbox is None:
+                    messagebox.showerror("Error", "No clothes detected in the image. Please choose another image.")
+                else:
+                    self.selected_image_path = util.crop_image(self.selected_image_path, bbox)
+                    paths = util.get_images_bdd(self.category, selected_brands, selected_sizes)
+                    result = util.get_similar_images(paths, self.selected_image_path, similar_count)
+                    self.show_results(result)
+
+    def show_results(self, results):
+        # Results Area
+        results_window = tk.Toplevel(self.root)
+        results_frame = tk.Frame(results_window)
+        results_frame.pack(pady=10)
+
+
+        # Display the chosen image
+        img = Image.open(self.selected_image_path)
+        img.thumbnail((self.width, self.height))  # Resize image
+        img = ImageTk.PhotoImage(img)
+        img_label = tk.Label(results_frame, image=img)
+        img_label.image = img  # Keep a reference to avoid garbage collection
+
+        frame = tk.Frame(results_frame)
+        frame.pack(side="left", padx=10)
+
+        # Add the image to the frame
+        img_label = tk.Label(frame, image=img, width=self.width, height=self.height)
+        img_label.pack()
+
+        cat = util.get_category_bdd(self.category)
+        category = "Catégorie : " + cat
+        # Add the description below the image
+        desc_label = tk.Label(frame, text=category, wraplength=self.width, justify="center")
+        desc_label.pack()
+
+        # Placeholder for results
+        results_display_frame = tk.Frame(results_frame)
+        results_display_frame.pack(side="left", padx=10)
+
+        images = []  # List to hold image references to avoid garbage collection
+
+        for index, img_path in enumerate(results):
+            img = Image.open(img_path[0])
+            img.thumbnail((self.width, self.height))  # Resize image
+            img = ImageTk.PhotoImage(img)
+            images.append(img)  # Append to the list to keep a reference
+            percent = round(img_path[1]*100, 2)
+            description = "Taux de similarité : " + str(percent) + "%"
+            size = "Taille : " + img_path[2]
+            brand = "Marque : " + img_path[3]
+
+            row = index % 2
+            col = index // 2
+            # Create a frame for each image and its description
+            frame = tk.Frame(results_display_frame, borderwidth=1, relief="solid")
+            frame.grid(row=row, column=col, padx=5, pady=5)
+
+            # Add the image to the frame
+            img_label = tk.Label(frame, image=img, width=self.width, height=self.height)
+            img_label.pack()
+
+            # Add the description below the image
+            desc_label_percent = tk.Label(frame, text=description, wraplength=self.width, justify="center")
+            desc_label_percent.pack()
+            desc_label_size = tk.Label(frame, text=size, wraplength=self.width, justify="center")
+            desc_label_size.pack()
+            desc_label_brand = tk.Label(frame, text=brand, wraplength=self.width, justify="center")
+            desc_label_brand.pack()
+
+        results_window.images = images  # Store the image references in the window to avoid garbage collection
+
+        results_window.mainloop()
+
+    def run(self, sizes, brands):
+        self.create_widgets(sizes, brands)
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    util.clean_destination()
+    sizes = util.get_sizes_bdd()
+    brands = util.get_brands_bdd()
+    app = ClothesFinderApp()
+    app.run(sizes=sizes, brands=brands)
+
---
+++ Interface/Utilities.py
@@ -0,0 +1,200 @@
+import mysql.connector
+import numpy as np
+from ultralytics import YOLO
+from PIL import Image
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.preprocessing import image
+from keras.models import Model
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+import shutil
+import cv2
+
+yolo_model_path = '../runs/detect/train6/weights/best.pt'
+destination_path = '../runs/find/'
+origin_path = '../data/cropped/'
+LIMIT = 100
+
+
+def delete_folder(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        print(f"The folder {folder_path} has been deleted.")
+    else:
+        print(f"The folder {folder_path} does not exist.")
+
+def clean_destination():
+    delete_folder(destination_path)
+    creer_fichiers(destination_path)
+
+def creer_fichiers(chemin_fichier):
+    '''Créer les dossier et fichiers nécessaires pour le fichier txt ou les images
+    chemin_fichier : str'''
+
+    dossier = os.path.dirname(chemin_fichier)
+    if not os.path.exists(dossier):
+        os.makedirs(dossier)
+
+def create_connection_bdd():
+    cnx = mysql.connector.connect(user='root', password='TO52',
+                                  host='localhost',
+                                  database='clothes_detection')
+    return cnx
+
+
+def construct_query(cat, brand, size):
+    query = "SELECT absolute_path FROM clothes "
+    where_category = f"WHERE clothes.id_category = {cat} "
+    where_brand = ""
+    where_size = ""
+    first = True
+
+    if brand != 'all':
+        query = query + "NATURAL JOIN brand "
+        for bd in brand:
+            if first:
+                where_brand = f"AND (brand = '{bd}' "
+                first = False
+            else:
+                where_brand = where_brand + f"OR brand = '{bd}' "
+        if len(brand) > 0:
+            where_brand = where_brand + ") "
+
+    first = True
+    if size != 'all':
+        query = query + "NATURAL JOIN size "
+        for sz in size:
+            if first:
+                where_size = f"AND (size = '{sz}' "
+                first = False
+            else:
+                where_size = where_size + f"OR size = '{sz}' "
+        if len(size) > 0:
+            where_size = where_size + ") "
+
+    query = query + where_category + where_brand + where_size + "ORDER BY RAND() LIMIT " + str(LIMIT)
+
+    return query
+
+
+def get_sizes_bdd():
+    cnx = create_connection_bdd()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT DISTINCT size FROM size")
+    sizes = cursor.fetchall()
+    sizes = [size[0] for size in sizes]
+    cnx.close()
+    return sizes
+
+def get_brands_bdd():
+    cnx = create_connection_bdd()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT DISTINCT brand FROM brand")
+    brands = cursor.fetchall()
+    brands = [brand[0] for brand in brands]
+    cnx.close()
+    return brands
+
+def get_size_and_brand(path):
+    cnx = create_connection_bdd()
+    cursor = cnx.cursor()
+    cursor.execute(f"SELECT size.size FROM clothes NATURAL JOIN size WHERE absolute_path = '{path}' ;")
+    size= cursor.fetchall()
+    cursor.execute(f"SELECT brand.brand FROM clothes NATURAL JOIN brand WHERE absolute_path = '{path}' ;")
+    brand= cursor.fetchall()
+    cnx.close()
+    return size[0][0], brand[0][0]
+
+def get_images_bdd(cat, brand, size):
+    cnx = create_connection_bdd()
+    cursor = cnx.cursor()
+    query = construct_query(cat, brand, size)
+    print(query)
+    cursor.execute(query)
+    paths = cursor.fetchall()
+    paths = [path[0] for path in paths]
+    correct_paths = [origin_path + path for path in paths]
+    cnx.close()
+    return correct_paths
+
+def get_category_bdd(cat):
+    cnx = create_connection_bdd()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT category FROM category WHERE id_category = " + str(cat))
+    category = cursor.fetchall()
+    cnx.close()
+    return category[0][0]
+
+def predict_category(image_path):
+    cat, bbox = None, None
+    model = YOLO(yolo_model_path)
+    results = model.predict(source=image_path, save=True)
+    for result in results:
+        if len(result.boxes) == 0:
+            return None, None
+        classe = result.boxes[0].cls  # class, (N, )
+        bounding_box = result.boxes[0].xyxy[0]
+        bbox = bounding_box.tolist()
+        cat = int(classe.item())
+    return cat, bbox
+
+def crop_image(image_path, bbox):
+    chemin_depart = image_path
+    chemin_arrivee = destination_path + 'cropped_image.jpg'
+    im = np.array(Image.open(chemin_depart))
+    im_trim = im[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])] #y1:y2, x1:x2
+    Image.fromarray(im_trim).save(chemin_arrivee)
+    return chemin_arrivee
+
+
+def extract_features(img_path, model):
+    # Extraction des caractéristiques avec VGG16
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_data = image.img_to_array(img)
+    img_data = np.expand_dims(img_data, axis=0)
+    img_data = preprocess_input(img_data)
+    vgg16_feature = model.predict(img_data)
+
+    # Extraction de l'histogramme de couleurs
+    img_bgr = cv2.imread(img_path)
+    img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+    hist_h = cv2.calcHist([img_hsv], [0], None, [50], [0, 256])
+    hist_s = cv2.calcHist([img_hsv], [1], None, [60], [0, 256])
+    hist_v = cv2.calcHist([img_hsv], [2], None, [60], [0, 256])
+    hist_h = cv2.normalize(hist_h, hist_h).flatten()
+    hist_s = cv2.normalize(hist_s, hist_s).flatten()
+    hist_v = cv2.normalize(hist_v, hist_v).flatten()
+    color_hist = np.concatenate((hist_h, hist_s, hist_v))
+
+    return vgg16_feature, color_hist
+
+def get_similar_images(paths, goal, limite):
+    similarities = [0 for i in range(len(paths))]
+    i = 0
+    base_model = VGG16(weights='imagenet')
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+    new_image_features, new_image_color_hist = extract_features(goal, model)
+    for path in paths:
+        feature, color_hist = extract_features(path, model)
+        similarity_features = cosine_similarity(new_image_features, feature).tolist()[0][0]
+        similarity_color = cv2.compareHist(new_image_color_hist, color_hist, cv2.HISTCMP_CORREL)
+        combined_similarity = (similarity_features + similarity_color) / 2
+        combined_similarity = round(combined_similarity, 4)
+        print(f"Similarité combinée : {combined_similarity}")
+        similarities[i] = combined_similarity
+        name = str(combined_similarity).replace("0.", "")
+        shutil.copy2(path, "./runs/find/" + name + '.jpg')
+        i += 1
+    indices = np.argsort(similarities)[::-1][:limite]
+    new_paths = [[paths[i], similarities[i]] for i in indices]
+    best_paths = get_infos_bdd(new_paths)
+    return best_paths
+
+def get_infos_bdd(paths):
+    for path in paths:
+        size, brand = get_size_and_brand(path[0].replace('data/cropped/', ""))
+        path.append(size)
+        path.append(brand)
+    print(paths)
+    return paths
+
